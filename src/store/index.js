141ca9337062e5ac5b87b9ae4a33a956356f0e87
Majeed Godhrawala <majeedgodhrawala@gmail.com>
2024-05-24 18:55:14 +0530

    Redux With Persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tasksReducer from "../reducers";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 
-const store = configureStore({
-  reducer: tasksReducer,
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, tasksReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
 });
 
-export default store;
+export const persistor = persistStore(store);
