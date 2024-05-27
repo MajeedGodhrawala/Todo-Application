2226867042dff8cfb4fb9bdae99a7e8cbc401850
Majeed Godhrawala <majeedgodhrawala@gmail.com>
2024-05-27 11:02:07 +0530

    "non-serializable" error solution

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import tasksReducer from "../reducers";
 import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
 const persistConfig = {
   key: "root",
@@ -12,6 +20,12 @@ const persistedReducer = persistReducer(persistConfig, tasksReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
